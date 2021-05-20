7a2b5cec581b0c7404d51df6b7705a53d32d761b
wudidada <hackerjiang@gmail.com>
2021-05-20 16:42:02 +0800

    add stop

---
+++ scrapyd/stop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+function stop {
+    process=$1
+    #杀进程
+    pids=$(ps -ef | grep "${process}$" | grep -v grep | awk '{print $2}')
+    for pid in $pids
+    do
+        echo "killed $pid"
+        kill -9 $pid
+    done
+}
+
+source ~/scrapyd/bin/activate
+
+stop "logparser"
+stop "scrapyd"
