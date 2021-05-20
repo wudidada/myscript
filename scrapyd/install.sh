5a371dac983281c56c67bad88f346e2071f49cf8
wudidada <hackerjiang@gmail.com>
2021-05-20 12:05:35 +0800

    update

--- scrapyd/install.sh
+++ scrapyd/install.sh
@@ -5,7 +5,8 @@ python3 -m venv scrapyd
 
 . scrapyd/bin/activate
 python3 -m pip install -U pip
-pip3 config set index-url https://mirrors.aliyun.com/pypi/simple/
+pip3 config set global.index-url https://mirrors.aliyun.com/pypi/simple/
 
+pip3 install scrapy
 pip3 install scrapyd
 pip3 install logparser
