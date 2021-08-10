66f0829bd5cd8d58593377a0f08d3c6a2137c9c9
wudidada <hackerjiang@gmail.com>
2021-08-10 15:58:13 +0800

    添加安装脚本

---
+++ scrapydweb/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd ~ || exit
+python3 -m venv scrapydweb
+
+. scrapydweb/bin/activate
+python3 -m pip install -U pip
+pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
+pip3 install scrapydweb
